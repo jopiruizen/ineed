55db28dc05707de3995b547fdaefe4a72a3fdec3
jopiruien <jopiruizen@yahoo.com>
2012-05-04 12:32:02 +0800

    iNeed first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ineed::Application.config.secret_token = 'f70193cba5ff989551b63a05321dd66ec41abfbe2ee582098d67aad338a2573735a71fb18fc30a0f337595a737d4d016b0f0e1ee8565f247d67e471cf3903f50'
